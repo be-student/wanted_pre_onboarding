6fc8f6be0d974f5b5a3947da451e02cb0a567f7f
Be-student <presentsong@naver.com>
2022-10-13 17:59:09 +0900

    fix: typeorm entity

--- src/infrastructure/typeormEntity/Company.entity.ts
+++ src/infrastructure/typeormEntity/Company.entity.ts
@@ -1,13 +1,10 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, OneToMany, PrimaryColumn } from 'typeorm';
 import { CompanyAdditional } from './CompanyAdditional.entity';
 import { DateEntity } from './Date.entity';
 
 @Entity()
 export class Company extends DateEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column()
+  @PrimaryColumn()
   name: string;
 
   @OneToMany(
