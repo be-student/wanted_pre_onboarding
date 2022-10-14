e0f596ea8a014c440fe5871cdc8dd9ea1d16701d
Be-student <presentsong@naver.com>
2022-10-14 18:56:22 +0900

    feat: add Announce Entity

--- src/modules/announce/announce.module.ts
+++ src/modules/announce/announce.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AnnounceService } from './announce.service';
 import { AnnounceController } from './announce.controller';
+import { Announce } from '@typeormEntity/Announce.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Announce])],
   controllers: [AnnounceController],
-  providers: [AnnounceService]
+  providers: [AnnounceService],
 })
 export class AnnounceModule {}
--- src/modules/announce/announce.service.spec.ts
+++ src/modules/announce/announce.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Announce } from '@typeormEntity/Announce.entity';
 import { AnnounceService } from './announce.service';
 
+const mockRepository = () => ({
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+});
 describe('AnnounceService', () => {
   let service: AnnounceService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AnnounceService],
+      providers: [
+        AnnounceService,
+        {
+          provide: getRepositoryToken(Announce),
+          useValue: mockRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<AnnounceService>(AnnounceService);
